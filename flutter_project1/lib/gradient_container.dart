958febeb3a7f4a578050232101d2cf901053571b
NaHid Hasan Meraj <nahid.meraj765@gmail.com>
2025-01-07 01:44:17 +0600

    adding instance variables; this, var & final

--- flutter_project1/lib/gradient_container.dart
+++ flutter_project1/lib/gradient_container.dart
@@ -1,20 +1,23 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_project1/style_text.dart';
+import 'package:flutter_project1/styled_text.dart';
+
+var startAlignment = Alignment.topLeft;
+var endAlignment = Alignment.bottomRight;
 class GradientContainer extends StatelessWidget {
   const GradientContainer({super.key});
 
   @override
   Widget build(context) {
     return Container(
-      decoration: const BoxDecoration(
+      decoration: BoxDecoration(
         gradient: LinearGradient(
-          colors: [Colors.deepPurple, Colors.blue],
-          begin: Alignment.topLeft,
-          end: Alignment.bottomRight,
+          colors: const [Colors.deepPurple, Colors.blue],
+          begin: startAlignment,
+          end: endAlignment,
         ),
       ),
       child: const Center(
-        child: StyledText(),
+        child: StyledText("Hello World!"),
       ),
     );
   }
